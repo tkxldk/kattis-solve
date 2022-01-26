a31041e8fc3e6cabde7e6b76a48578ad1c8048b8
Q <89120989+XYRQ0@users.noreply.github.com>
2022-01-26 15:37:27 +0700

    Add files via upload

---
+++ Quadrant Selection.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int x, y;
+	scanf("%d %d", &x, &y);
+	
+	if(x > 0 && y > 0) puts("1");
+	else if(x < 0 && y > 0) puts("2");
+	else if(x < 0 && y < 0) puts("3");
+	else puts("4");
+	
+	return 0;
+}
+
