c6f7d5daf7ccd7590f71796eac302e798635164c
Q <89120989+XYRQ0@users.noreply.github.com>
2022-01-26 17:49:44 +0700

    Update Number Fun.cpp

--- Number Fun.cpp
+++ Number Fun.cpp
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main(){
-	int n, a, b, c;
+	int n;
+	double a, b, c;
 	scanf("%d",&n);
 	
 	for(int i = 0; i < n; i++){
-		scanf("%d %d %d", &a, &b, &c);
+		scanf("%lf %lf %lf", &a, &b, &c);
 		
-		if(a + b == c || a - b == c || b - a == c || a * b == c || a / b == c || b / a == c) puts("Possible");
-		else puts("Impossible");	
+		if(a + b == c || a - b == c || b - a == c || a * b == c || a / b == c || b / a == c){
+			puts("Possible");	
+		}
+		else{
+			puts("Impossible");	
+		}
 	}
 	
 	return 0;
