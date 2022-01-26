435071a4322f0d3295a6a45cba15c0a7385b8483
Q <89120989+XYRQ0@users.noreply.github.com>
2022-01-26 17:50:01 +0700

    Update Number Fun.cpp

--- Number Fun.cpp
+++ Number Fun.cpp
@@ -8,12 +8,8 @@ int main(){
 	for(int i = 0; i < n; i++){
 		scanf("%lf %lf %lf", &a, &b, &c);
 		
-		if(a + b == c || a - b == c || b - a == c || a * b == c || a / b == c || b / a == c){
-			puts("Possible");	
-		}
-		else{
-			puts("Impossible");	
-		}
+		if(a + b == c || a - b == c || b - a == c || a * b == c || a / b == c || b / a == c) puts("Possible");	
+		else puts("Impossible");	
 	}
 	
 	return 0;
